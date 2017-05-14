9601346254fd9e99bb35527db2454d89dab32477
xu.wang <867103701@qq.com>
2017-05-14 13:04:55 +0800

    ..

--- app/src/main/java/com/demo/drawpaintview/paint/util/DrawUtil.java
+++ app/src/main/java/com/demo/drawpaintview/paint/util/DrawUtil.java
@@ -26,39 +26,9 @@ public class DrawUtil {
      * @param startP 开始点
      * @param endP   结束点
      * @param paint  画笔
-     * @author 孙潮宗 计算箭头的算法,复制自pc客户端
      */
     public static void drawArrow(Canvas canvas, PointF startP, PointF endP, Paint paint) {
-        float lineAngle = (float) Math.atan((endP.y - startP.y) / (endP.x - startP.x));
-        float angleA1 = lineAngle - arrowAngle;
-        float angleA2 = lineAngle + arrowAngle;
-        int adjust = (endP.x >= startP.x) ? -1 : 1;
-        float a1X = (float) (endP.x + adjust * arrowLength * Math.cos(angleA1));
-        float a1Y = (float) (endP.y + adjust * arrowLength * Math.sin(angleA1));
-        float a2X = (float) (endP.x + adjust * arrowLength * Math.cos(angleA2));
-        float a2Y = (float) (endP.y + adjust * arrowLength * Math.sin(angleA2));
-        PointF _arrowPoint1 = new PointF(a1X, a1Y);
-        PointF _arrowPoint2 = new PointF(a1X + (a2X - a1X) / 3, a1Y + (a2Y - a1Y) / 3);
-        PointF _arrowPoint3 = new PointF(a1X + (a2X - a1X) * 2 / 3, a1Y + (a2Y - a1Y) * 2 / 3);
-        PointF _arrowPoint4 = new PointF(a2X, a2Y);
-        double minPointDistance = arrowLength * Math.cos(arrowAngle);
-        PointF[] points;
-        if (distance(startP, endP) > minPointDistance) {
-            points = new PointF[]{startP, _arrowPoint2, _arrowPoint1, endP, _arrowPoint4, _arrowPoint3, startP};
-        } else {
-            points = new PointF[]{_arrowPoint1, endP, _arrowPoint4, _arrowPoint1};
-        }
-        Path path = new Path();
-        for (int i =0; i < points.length;i++){
-            if (i == 0){
-                path.moveTo(points[0].x,points[0].y);
-            } else {
-                path.lineTo(points[i].x,points[i].y);
-            }
-        }
-        paint.setStyle(Paint.Style.FILL);
-        paint.setAntiAlias(true);
-        canvas.drawPath(path, paint);
+
     }
 
     private static double distance(PointF startP, PointF endP) {
@@ -96,7 +66,6 @@ public class DrawUtil {
 
     public static String getTouchLineId(float x, float y, TransPointF transPointF, ArrayList<LineInfo> mPaintLines) {
         PointF pointF = transPointF.display2Logic(x, y);
-        int area = 3;
         for (int i = 0; i < mPaintLines.size(); i++) {
             Path path = new Path();
             LineInfo lineInfo = mPaintLines.get(i);
@@ -117,7 +86,7 @@ public class DrawUtil {
             path.computeBounds(bounds, true);
             Region region = new Region();
             region.setPath(path, new Region((int) bounds.left, (int) bounds.top, (int) bounds.right, (int) bounds.bottom));
-            if (region.quickReject(new Rect((int)pointF.x - 2,(int)pointF.y- 2,(int)pointF.x + 2,(int) pointF.y+ 2))) {
+            if (!region.quickReject(new Rect((int)pointF.x - 2,(int)pointF.y- 2,(int)pointF.x + 2,(int) pointF.y+ 2))) {
                 return lineInfo.getLineId();
             }
         }
